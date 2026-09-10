c58a04a16f2d18373ada453878fb67aeefd8cc03
HerbertTrindade <herbert.santos77@gmail.com>
2026-09-09 22:00:07 -0300

    Adicionando JPA, modificando entity com anotacions

--- src/main/java/br/com/Spring/Study/Entity/TaskEntity.java
+++ src/main/java/br/com/Spring/Study/Entity/TaskEntity.java
@@ -1,55 +1,34 @@
 package br.com.Spring.Study.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tab_jobs")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class TaskEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private UUID id;
+
     @Column(name = "Name")
     private String name;
+
     @Column(name = "Level")
     private int level;
+
     @Column(name = "Worker")
     @OneToMany(mappedBy = "task")
     private List<WorkerEntity> workers;
 
-    protected TaskEntity() {
-    }
 
-    public TaskEntity(String name, int difficulty) {
-        this.name = name;
-        this.level = difficulty;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
 }
--- src/main/java/br/com/Spring/Study/Entity/WorkerEntity.java
+++ src/main/java/br/com/Spring/Study/Entity/WorkerEntity.java
@@ -1,6 +1,9 @@
 package br.com.Spring.Study.Entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import java.math.BigDecimal;
@@ -9,80 +12,33 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "tb_registration")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class WorkerEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private UUID id;
-    @Column(name = "Name")
+
+    @Column(name = "Name",nullable = false)
     private String name;
-    @Column(name = "Company")
+
+    @Column(name = "Company",nullable = false)
     private String company;
-    @Column(name = "Position")
+
+    @Column(name = "Position",nullable = false)
     private String position;
-    @Column(name = "Salary")
+
+    @Column(name = "Salary",nullable = false)
     private BigDecimal salary;
-    @Column(name = "DateStart")
+
+    @Column(name = "DateStart",nullable = false)
     private LocalDate dateStart;
 
-    public WorkerEntity(){
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private TaskEntity task;
 
     }
-
-    public WorkerEntity(String name, String company, String position, BigDecimal salary, LocalDate dateStart) {
-        this.name = name;
-        this.company = company;
-        this.position = position;
-        this.salary = salary;
-        this.dateStart = dateStart;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public BigDecimal getSalary() {
-        return salary;
-    }
-
-    public void setSalary(BigDecimal salary) {
-        this.salary = salary;
-    }
-
-    public LocalDate getDateStart() {
-        return dateStart;
-    }
-
-    public void setDateStart(LocalDate dateStart) {
-        this.dateStart = dateStart;
-    }
-}
