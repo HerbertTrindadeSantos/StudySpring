32522a6611f47c1e19e1d7f414467dfcc20412da
HerbertTrindade-dev <herbert.santos77@gmail.com>
2026-09-15 15:16:18 -0300

    Criacao de enum para representar o nivel do cargo

---
+++ src/main/java/br/com/Spring/Study/Entity/enums/TaskLevel.java
@@ -0,0 +1,8 @@
+package br.com.Spring.Study.entity.enums;
+
+
+public enum TaskLevel {
+    LOW,
+    MEDIUM,
+    HIGH;
+}
---
+++ src/main/java/br/com/Spring/Study/Entity/enums/WorkerLevel.java
@@ -0,0 +1,7 @@
+package br.com.Spring.Study.entity.enums;
+
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
